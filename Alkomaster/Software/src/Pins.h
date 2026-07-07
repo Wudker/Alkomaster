f8e9a1c7d83960f25e49f7b4d70b0bc0918b719b
Bartłomiej Dusza <duszyczka2003@o2.pl>
2026-07-07 23:58:13 +0200

    Pin update

--- Alkomaster/Software/src/Pins.h
+++ Alkomaster/Software/src/Pins.h
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 #define ADC_PIN_SENSOR 4
 #define Wake_up_pin 2
-#define Sensor_power_pin 3
+#define Sensor_power_pin 8
 #define Display_power_pin 21
 #define Battery_voltage_pin 0
 
